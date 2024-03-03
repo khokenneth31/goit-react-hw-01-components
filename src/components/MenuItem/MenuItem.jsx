383ada3e69dae6ab33b76e26c03ee6e5e8a84872
khokenneth31 <khokenneth26@gmail.com>
2024-03-03 13:53:18 +0800

    1st repo

---
+++ src/components/MenuItem/MenuItem.jsx
@@ -0,0 +1,27 @@
+import React, { Fragment } from 'react';
+
+const MenuItem = () => {
+    return (
+      <Fragment
+  /*      style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101'
+        }}*/
+      >
+        <h1>React homework template</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro magnam,
+             voluptatibus placeat laboriosam debitis harum eveniet nostrum inventore 
+             expedita totam iusto nemo ad eligendi dolorem nihil! A, 
+             asperiores? Dolorem, amet.</p>
+             <div>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum possimus fugit, eligendi sit pariatur nesciunt amet facilis quo unde saepe neque soluta? Vel temporibus nulla, amet commodi earum quibusdam accusantium!
+             </div>
+             <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde totam maiores quaerat, voluptates neque ut provident officia cumque harum nam delectus temporibus optio, deleniti, perspiciatis natus dolores corporis. Quia, dolor!</div>
+      </Fragment>
+    );
+  };
+  export default MenuItem;
